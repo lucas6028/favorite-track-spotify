986aef052b0c349b77f74a7401ae52015ad80b78
haoping6028 <lucas60303@gmail.com>
2024-08-10 12:03:55 +0800

    modify redirect uri in to vercel

--- src/auth/Redirect.ts
+++ src/auth/Redirect.ts
@@ -10,7 +10,8 @@ export async function redirectToAuthCodeFlow(clientId: string) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  // params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", "https://spotify-api-pi.vercel.app/callback");
   // params.append("scope", "user-read-private user-read-email");
   params.append("scope", "user-top-read");
   params.append("code_challenge_method", "S256");
